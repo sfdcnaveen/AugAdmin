551bc4c0d4f819b3495688aa30bebf07c1d8d0be
Naveen Kumar Pasupuleti <pasupulatink@gmail.com>
2023-08-29 07:02:16 +0530

    Uncommented lines in PostFix.java

--- src/basics/PostFix.java
+++ src/basics/PostFix.java
@@ -3,13 +3,13 @@ package basics;
 public class PostFix {
 	public static void main(String[] args) {
 		
-//		int x = 10;
-//		int y = 20;
-//		System.out.println(x++);
-//		System.out.println(x);
-//		System.out.println(x++);
-//		System.out.println(x);
-//		System.out.println(x++ + y++ + x++); //10 + 20 + 11 = 41
+		int x = 10;
+		int y = 20;
+		System.out.println(x++);
+		System.out.println(x);
+		System.out.println(x++);
+		System.out.println(x);
+		System.out.println(x++ + y++ + x++); //10 + 20 + 11 = 41
 		
 //		int a=5, b=6;
 //		System.out.println(a++ + b++ - a-- + b++ - a-- + b++ + a++); // 5 + 6 - 6 + 7 - 5 + 8 + 4 
