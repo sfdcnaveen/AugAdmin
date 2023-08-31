3236401dc314e2b74d8fee58186fd4a27aac4fb2
Naveen Kumar Pasupuleti <pasupulatink@gmail.com>
2023-08-31 06:32:59 +0530

    Adding loop statements

---
+++ src/basics/LoopingStatements.java
@@ -0,0 +1,11 @@
+package basics;
+
+public class LoopingStatements {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("iteration:"+i);
+            
+        }
+    }
+    
+}
