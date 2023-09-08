11b1f5721f8be457bfdb8c309679e644e08120fa
Naveen Kumar Pasupuleti <pasupulatink@gmail.com>
2023-09-08 06:00:13 +0530

    Added Break, Continue statements, new pattern prog

--- src/basics/LoopingStatements.java
+++ src/basics/LoopingStatements.java
@@ -6,6 +6,20 @@ public class LoopingStatements {
             System.out.println("iteration:"+i);
             
         }
+        for (int j = 1; j <= 10; j++) {
+            if (j==4) {
+                break;                      //Break the loop
+            }
+            System.out.println("iteration:"+j);
+            
+        }
+        for (int k = 1; k <= 10; k++) {
+            if (k==4) {
+                continue;                      //skips the iteration step which skips printing 4 we can use OR statement in condition but not AND
+            }
+            System.out.println(k);
+            
+        }
     }
     
 }
---
+++ src/basics/Pattern1.java
@@ -0,0 +1,13 @@
+package basics;
+
+public class Pattern1 {
+    public static void main(String[] args) {
+        for(int i=1; i<=10; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    
+}
