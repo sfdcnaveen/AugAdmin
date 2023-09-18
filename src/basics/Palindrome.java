1798494cb8709ef43c579538ff5c0d183cc392d1
Naveen Kumar Pasupuleti <pasupulatink@gmail.com>
2023-09-18 06:25:16 +0530

    New programs

---
+++ src/basics/Palindrome.java
@@ -0,0 +1,25 @@
+package basics;
+
+import java.util.Scanner;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int n = sc.nextInt();
+        int r,sum=0,temp;
+
+        temp = n;
+
+        while(n>0)
+        {
+            r=n%10;
+            sum = (sum*10)+r;
+            n=n/10;
+        }
+        if(temp == sum)
+            System.out.println("Palindrome number");
+        else
+            System.out.println("Not a palindrome");
+    }
+}
